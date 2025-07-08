0d67848af85f1859fc473fbe73d262e5cc9e6b48
Vinicius Emidio Bosi <viniemidiobosi@gmail.com>
2025-07-08 14:29:37 -0300

    feat: add exclusion for tailscale requests

--- manage-port-forwarding.sh
+++ manage-port-forwarding.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-    then echo "This script must be run as root"
-    exit
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
 fi
 
 saveIptables() {
@@ -10,66 +10,65 @@ saveIptables() {
 }
 
 installIptablesPersistent() {
-    apt install iptables-persistent
+    apt install -y iptables-persistent
 }
 
 manageForward() {
-    # -A: append a rule
-    # -D: delete a rule
     local operation=$1
 
     read -p "Enter protocol (tcp/udp): " protocol
     read -p "Enter local port: " localPort
-    read -p "Enter remote ip: " ip
-    read -p "Enter remote port: " remotePort
+    read -p "Destination IP: " ip
+    read -p "Destination port: " remotePort
 
-    iptables -t nat $operation PREROUTING -p $protocol --dport $localPort -j DNAT --to-destination $ip:$remotePort
-    iptables -t nat -A POSTROUTING -j MASQUERADE
+    if [ "$operation" = "-A" ]; then
+        # Redirects only if origin is not Tailscale (100.64.0.0/10)
+        iptables -t nat -A PREROUTING ! -s 100.64.0.0/10 -p "$protocol" --dport "$localPort" -j DNAT --to-destination "$ip:$remotePort"
+        iptables -t nat -A POSTROUTING -j MASQUERADE
+    else
+        # Remove rule
+        iptables -t nat -D PREROUTING -p "$protocol" --dport "$localPort" -j DNAT --to-destination "$ip:$remotePort"
+    fi
 }
 
 listForward() {
-    iptables -t nat -L PREROUTING
+    iptables -t nat -L PREROUTING -n -v
 }
 
 enableIpForwarding() {
     isIpForwardingEnabled=$(cat /proc/sys/net/ipv4/ip_forward)
 
-    if [ $isIpForwardingEnabled -eq 1 ]
-    then
+    if [ isIpForwardingEnabled -eq 1 ]; then
         echo "IP forwarding is already enabled"
-        return
+    else
+        echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+        sysctl -p
+        echo "IP forwarding enabled"
     fi
-
-    echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-    sysctl -p
-    sysctl --system
-
-    echo "IP forwarding has been enabled"
 }
 
-title="Hello! This is a script manage port forwarding using iptables"
-title=""
-prompt="Pick an option:"
+title="Port Forwarding Manager (iptables)"
+prompt="Choose an option:"
 options=(
-    "Add an new port forwarding rule"
-    "Remove an existing port forwarding rule"
-    "List all port forwarding rules"
+    "Add redirect rule"
+    "Remove redirect rule"
+    "List redirect rules"
     "Save iptables rules"
     "Install iptables-persistent"
-    "Enable IP forwarding"
+    "Enable IP Forwarding"
 )
 
 echo "$title"
 PS3="$prompt "
-select opt in "${options[@]}" "Quit"; do 
+select opt in "${options[@]}" "Sair"; do 
     case "$REPLY" in
-    1) manageForward -A;;
-    2) manageForward -D;;
-    3) listForward;;
-    4) saveIptables;;
-    5) installIptablesPersistent;;
-    6) enableIpForwarding;;
-    $((${#options[@]}+1))) echo "Goodbye!"; break;;
-    *) echo "Invalid option. Try another one.";continue;;
+        1) manageForward -A ;;
+        2) manageForward -D ;;
+        3) listForward ;;
+        4) saveIptables ;;
+        5) installIptablesPersistent ;;
+        6) enableIpForwarding ;;
+        $((${#options[@]}+1))) echo "Saindo..."; break ;;
+        *) echo "Opção inválida. Tente novamente." ;;
     esac
 done
